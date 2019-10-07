8e3ca6518c0f507153f78e5ee0be63f8121299cb
Felipe Acosta <facosta0787@gmail.com>
2019-10-07 16:20:53 -0500

    Added movies pagination

--- src/movies/controller.js
+++ src/movies/controller.js
@@ -1,6 +1,7 @@
 const { Movies } = require('../lib/database')
+const { paginateResults } = require('./utils')
 
-const getMovies = async (root, args, { req }) => {
+const getMovies = async (_, __, { req }) => {
   console.log(req.headers.origin)
   try {
     const movies = await Movies.findAll({ raw: true })
@@ -11,7 +12,7 @@ const getMovies = async (root, args, { req }) => {
   }
 }
 
-const getMovie = async (root, args) => {
+const getMovie = async (_, args) => {
   try {
     const movie = await Movies.findOne({
       where: { id: args.id }
@@ -23,7 +24,29 @@ const getMovie = async (root, args) => {
   }
 }
 
+const getMoviesPaginated = async (_, { pageSize = 20, after }) => {
+  const allMovies = await Movies.findAll({
+    order: [['year', 'DESC']],
+    raw: true
+  })
+
+  const movies = paginateResults({
+    after,
+    pageSize,
+    results: allMovies
+  })
+
+  return {
+    movies,
+    cursor: movies.length ? movies[movies.length - 1].id : null,
+    hasMore: movies.length
+      ? movies[movies.length - 1].id !== allMovies[allMovies.length - 1].id
+      : false
+  }
+}
+
 module.exports = {
   getMovie,
-  getMovies
+  getMovies,
+  getMoviesPaginated
 }
--- src/movies/movies.resolver.js
+++ src/movies/movies.resolver.js
@@ -3,6 +3,7 @@ const controller = require('./controller')
 module.exports = {
   Query: {
     getMovies: controller.getMovies,
-    getMovie: controller.getMovie
+    getMovie: controller.getMovie,
+    movies: controller.getMoviesPaginated
   }
 }
